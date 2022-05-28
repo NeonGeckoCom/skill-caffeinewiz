1f3d4fd4e9c26f301d031a1dbc202a184ddb91ff
Daniel McKnight <daniel@neon.ai>
2022-05-27 18:11:50 -0700

    Remove fs_path overrides in tests

--- test/test_skill.py
+++ test/test_skill.py
@@ -43,9 +43,7 @@ class TestSkill(unittest.TestCase):
         if not exists(cls.test_fs):
             mkdir(cls.test_fs)
 
-        # Override the configuration and fs paths to use the test directory
-        cls.skill.local_config = get_neon_local_config(cls.test_fs)
-        cls.skill.user_config = get_neon_user_config(cls.test_fs)
+        # Override fs paths to use the test directory
         cls.skill.settings_write_path = cls.test_fs
         cls.skill.file_system.path = cls.test_fs
         cls.skill._init_settings()
